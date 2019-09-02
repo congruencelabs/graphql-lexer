5e39a2e985ff7dbb2aa7a4beea22aebdbd0b1825
Aditya Pratap Singh <adisinghrajput@gmail.com>
2019-09-03 00:03:07 +0200

    Add positions to token

--- token.go
+++ token.go
@@ -1,27 +1,27 @@
 package graphql_lexer
 
-const 	(
-	EOF          = "EOF"
-	ILLEGAL      = "ILLEGAL"
-	IDENTIFIER   = "IDENTIFIER"
-	COLON        = ":"
-	COMMA        = ","
-	LPAREN       = "("
-	RPAREN       = ")"
-	LBRACE       = "{"
-	RBRACE       = "}"
-	LBRACKET     = "["
-	RBRACKET     = "]"
-	EQUALS       = "="
-	BANG         = "!"
-	DOLLAR       = "$"
-	SPREAD       = "..."
-	AT           = "@"
-	PIPE         = "|"
-	INT          = "Int"
-	STRING       = "String"
-	FLOAT        = "Float"
-	BLOCKSTRING  = "BlockString"
+const (
+	EOF         = "EOF"
+	ILLEGAL     = "ILLEGAL"
+	IDENTIFIER  = "IDENTIFIER"
+	COLON       = ":"
+	COMMA       = ","
+	LPAREN      = "("
+	RPAREN      = ")"
+	LBRACE      = "{"
+	RBRACE      = "}"
+	LBRACKET    = "["
+	RBRACKET    = "]"
+	EQUALS      = "="
+	BANG        = "!"
+	DOLLAR      = "$"
+	SPREAD      = "..."
+	AT          = "@"
+	PIPE        = "|"
+	INT         = "Int"
+	STRING      = "String"
+	FLOAT       = "Float"
+	BLOCKSTRING = "BlockString"
 
 	DIRECTIVE    = "directive"
 	ENUM         = "enum"
@@ -41,29 +41,30 @@ const 	(
 type TokenType string
 
 type Token struct {
-	Type    TokenType
-	Literal string
+	Type         TokenType
+	Literal      string
+	Position     int
+	ReadPosition int
 }
 
 var tokenKeywordsMap = map[string]TokenType{
-	"...": SPREAD,
-	"enum": ENUM,
-	"extend": EXTEND,
-	"fragment": FRAGMENT,
-	"input": INPUT,
-	"interface": INTERFACE,
-	"query": QUERY,
-	"mutation": MUTATION,
-	"scalar": SCALAR,
-	"schema": SCHEMA,
+	"...":          SPREAD,
+	"enum":         ENUM,
+	"extend":       EXTEND,
+	"fragment":     FRAGMENT,
+	"input":        INPUT,
+	"interface":    INTERFACE,
+	"query":        QUERY,
+	"mutation":     MUTATION,
+	"scalar":       SCALAR,
+	"schema":       SCHEMA,
 	"subscription": SUBSCRIPTION,
-	"type": TYPE,
-	"union": UNION,
-
-	"Query": QUERY,
-	"Int": INT,
-	"String": STRING,
-	"Float": FLOAT,
+	"type":         TYPE,
+	"union":        UNION,
+	"Query":        QUERY,
+	"Int":          INT,
+	"String":       STRING,
+	"Float":        FLOAT,
 }
 
 func LookupIdentifier(ident string) TokenType {
@@ -72,5 +73,3 @@ func LookupIdentifier(ident string) TokenType {
 	}
 	return IDENTIFIER
 }
-
-
